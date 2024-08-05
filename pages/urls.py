6158c9e3e1f922af16ac02d45e496211230611b0
spockmay <spockmay@gmail.com>
2024-08-05 14:55:29 -0400

    override default login/logout pages

--- pages/urls.py
+++ pages/urls.py
@@ -7,8 +7,8 @@ from .views import home_page_view, home_page_template, login_template, secret_te
 urlpatterns = [
         path("", include("allauth.urls")),
         path("", home_page_template),
-        path("login", login_template, name='login'),
-	path('logout', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+        path("accounts/login/", login_template, name='login'),
+	path('accounts/logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
 	path('secret', secret_template)
         ]
 
